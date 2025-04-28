f93200774c923add97c0faf4c0250ea2f3f576d9
Ricardo <ricardohhammer.meet@gmail.com>
2025-04-27 22:09:41 -0400

    Initial Commit

---
+++ AccessibleSumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibleSumApp
+//
+//  Created by user277150 on 4/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
